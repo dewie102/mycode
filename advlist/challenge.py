00abee2f5ef2d1a3244b3ac67d1dfe339e6ea4e9
dewie102 <zack.romasz@tlgcohort.com>
2023-10-31 19:26:07 +0000

    List, Input and concat challenge complete

---
+++ advlist/challenge.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import random
+
+def main():
+    wordbank= ["indentation", "spaces"] 
+    tlgstudents=["Bert", "Angel", "Chandan", "Chris", "Gen", "Jojo", "Joseph", "Robert", "Sar", "Zack"]
+
+    wordbank.append(4)
+
+    max_number = 10
+
+    num_str = input(f"Please enter a number from 1 to {max_number}: ")
+
+    try:
+        num = int(num_str) - 1
+    except ValueError:
+        print("Please enter a proper number")
+        return;
+
+    if 0 > num or num > max_number - 1:
+        print("Pick a number inside the proper range")
+        return;
+
+    random.shuffle(tlgstudents)
+
+    student_name = tlgstudents[num]
+
+    print(f"{student_name} always uses {wordbank[-1]} {wordbank[1]} to indent.")
+
+if __name__ == "__main__":
+    main()
